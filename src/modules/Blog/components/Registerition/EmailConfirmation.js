a6b1d4cd26b61d8baeb13951090fc72462e5950b
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-04-03 17:28:47 +0200

    Added css to email confirmation component

--- src/modules/Blog/components/Registerition/EmailConfirmation.js
+++ src/modules/Blog/components/Registerition/EmailConfirmation.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 import { FaEnvelopeOpen } from "react-icons/fa";
 
 
-const Confirmation = () =>{
-    return(
-        <main className="email-confirmation">
-            <div className="container">
-                <div class="jumbotron text-center email-confirmation-body">
-                    <p className="lead">لقد أرسلنا بريدًا إلكترونيًا إلى <Link to='mailto:moussaebrahem32@gmail.com' className="sent-email">moussaebrahem@gmail.com</Link></p>
+const Confirmation = () => {
+    return (
+        <main class="component-wrapper">
+            <div className="container home">
+                <div class="jumbotron text-center bg-second">
+                    <p className="lead">لقد أرسلنا بريدًا إلكترونيًا إلى <Link to='mailto:moussaebrahem32@gmail.com' className="text-lightgreen">moussaebrahem@gmail.com</Link></p>
                     <p>يرجى التحقق من بريدك الإلكتروني والتحقق من حسابك, للمتابعة.</p>
-                    <FaEnvelopeOpen size={'10em'} className="d-block mx-auto py-3"/>
+                    <FaEnvelopeOpen size={'10em'} className="d-block mx-auto py-3" />
                     <small>لم تتلق البريد الإلكتروني؟</small>
-                    <small><button className="resend-email-button d-block mx-auto pt-3">إعادة إرسال البريد الإلكتروني</button></small>
+                    <small><a className="text-lightgreen d-block mx-auto pt-3">إعادة إرسال البريد الإلكتروني</a></small>
                 </div>
             </div>
         </main>
