307697ee2846ce50331c04d1ccefd3518fb479b3
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-04-03 18:00:44 +0200

    Added css to sms-confirmation

--- src/modules/Blog/components/Registerition/SmsConfirmation.js
+++ src/modules/Blog/components/Registerition/SmsConfirmation.js
@@ -3,23 +3,23 @@ import { FaCheckCircle } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 
-const SmsConfirmation = () =>{
-    return(
-        <main className="phone-confirmation">
-            <div className="container">
-                <div className="jumbotron text-center phone-confirmation-body">
+const SmsConfirmation = () => {
+    return (
+        <main class="component-wrapper">
+            <div className="container home">
+                <div className="jumbotron text-center py-4 bg-second">
                     <nav aria-label="breadcrumb">
-                        <ol className="breadcrumb custom-breadcrumb">
-                            <li className="breadcrumb-item"><FaCheckCircle className="check-progress ml-2" size={'1.5em'}/> التحقق من البريد الإلكتروني</li>
+                        <ol className="breadcrumb bg-black">
+                            <li className="breadcrumb-item py-1"><FaCheckCircle className="check-progress ml-2" size={'1.5em'} /> التحقق من البريد الإلكتروني</li>
                         </ol>
                     </nav>
-                    <div className="jumbotron jumbotron-fluid phone-confirmation-form rounded">
+                    <div className="jumbotron jumbotron-fluid bg-black rounded pt-4">
                         <label className="d-block text-right box-title">التحقق من الموبيل</label>
                         <div className="container">
                             <div className="row">
-                                <div className="col-md-6 mx-auto">
-                                    <p>الرجاء إدخال رمز التحقق الذي أرسلناه إلى +201095122777</p>     
-                                    <input type="text" className="form-control" placeholder="كود التحقق"/>                
+                                <div className="col-md-6 mx-auto px-4">
+                                    <p>الرجاء إدخال رمز التحقق الذي أرسلناه إلى +201095122777</p>
+                                    <input type="text" className="form-control bg-white text-dark py-4" placeholder="كود التحقق" />
                                 </div>
                             </div>
                             <button className="btn btn-lightgreen my-4 d-block mx-auto">إرسال</button>
