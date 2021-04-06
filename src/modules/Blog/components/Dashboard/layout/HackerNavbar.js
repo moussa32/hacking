636908d8e762af2a96305e7f623e5169a9a712bb
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-04-06 17:57:16 +0200

    Added dropdown manu

--- src/modules/Blog/components/Dashboard/layout/HackerNavbar.js
+++ src/modules/Blog/components/Dashboard/layout/HackerNavbar.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Link, useRouteMatch } from "react-router-dom";
+import { MdEmail } from "react-icons/md";
+import { BsBellFill } from "react-icons/bs";
+import { IoIosArrowDown } from "react-icons/io";
+import { HackerImage } from "../../../../../assets/index";
+
+
+
 
 import { WhiteLogo } from "../../../../../assets/index";
 
@@ -7,6 +14,7 @@ const HackerNavbar = ({ currentPathname }) => {
     let { path } = useRouteMatch();
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isDropDown, setIsDropDown] = useState(false);
     const [activeTab, setActiveTab] = useState("main");
 
     useEffect(() => {
@@ -22,6 +30,7 @@ const HackerNavbar = ({ currentPathname }) => {
     }, [currentPathname]);
 
     const toggle = () => setIsOpen(!isOpen);
+    const toggleDropDown = () => setIsDropDown(!isDropDown);
 
     return (
         <nav className="navbar navbar-expand-lg navbar-dark blog-nav">
@@ -79,7 +88,31 @@ const HackerNavbar = ({ currentPathname }) => {
             </Link>
                     </li>
                 </ul>
-                <ul className="navbar-nav mr-auto">
+                <ul className="navbar-nav mr-auto sub-hacker-list">
+                    <li className='nav-item nav-icon' id="messages">
+                        <Link className="nav-link" to={`${path}/leaderboard`}>
+                            <MdEmail className="text-lightgreen" size={'1.8rem'} />
+                        </Link>
+                    </li>
+                    <li className='nav-item nav-icon' id="alerts">
+                        <Link className="nav-link" to={`${path}/leaderboard`}>
+                            <BsBellFill className="text-lightgreen" size={'1.5rem'} />
+                        </Link>
+                    </li>
+                    <li className="nav-item dropdown" onClick={toggleDropDown}>
+                        <button className="nav-link dropdown-toggle d-none d-sm-inline-block border-0 bg-0 bg-transparent" id="hacker-profile" data-toggle="dropdown" aria-expanded="false">
+                            <img src={HackerImage} className="hacker-avatar img-fluid rounded-circle mr-1" alt="Chris Wood" />
+                            <IoIosArrowDown className="text-lightgreen mr-2" size={'1.3rem'} />
+                        </button>
+                        <div className={`dropdown-menu ${isDropDown ? "show" : ""}`}>
+                            <a className="dropdown-item" href="pages-profile.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user align-middle mr-1"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> Profile</a>
+                            <a className="dropdown-item" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-pie-chart align-middle mr-1"><path d="M21.21 15.89A10 10 0 1 1 8 2.83"></path><path d="M22 12A10 10 0 0 0 12 2v10z"></path></svg> Analytics</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="pages-settings.html">Settings &amp; Privacy</a>
+                            <a className="dropdown-item" href="#">Help</a>
+                            <a className="dropdown-item" href="#">تسجيل الخروج</a>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </nav>
