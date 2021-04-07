30d52d5361281ab35601016ccd4ec23a57cbe21f
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-04-07 15:40:19 +0200

    Added api url for devlopment

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -1,3 +1,6 @@
 //base url
 export const baseUrl = "https://osos.pythonanywhere.com";
 export const apiUrl = "https://osos.pythonanywhere.com/api/v1";
+
+export const dvbaseUrl = "http://bugbounty.pythonanywhere.com";
+export const dvbaseUrl = "http://bugbounty.pythonanywhere.com/api/v1";
