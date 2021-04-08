8996b0928812a965b04ff003307ad63e6a660455
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-04-08 08:18:05 +0200

    Update api protocol

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -2,5 +2,5 @@
 export const baseUrl = "https://osos.pythonanywhere.com";
 export const apiUrl = "https://osos.pythonanywhere.com/api/v1";
 
-export const dvbaseUrl = "http://bugbounty.pythonanywhere.com";
-export const dvApiUrl = "http://bugbounty.pythonanywhere.com/api/v1";
+export const dvbaseUrl = "https://bugbounty.pythonanywhere.com";
+export const dvApiUrl = "https://bugbounty.pythonanywhere.com/api/v1";
