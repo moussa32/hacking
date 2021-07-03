293344d272124085e1a137bc7111bf3024469ea0
Moussa Ibrahim <moussaebrahem32@gmail.com>
2021-07-03 13:56:37 +0300

    Test

--- src/api/Constants.js
+++ src/api/Constants.js
@@ -1,6 +1,6 @@
 //base url
-export const baseUrl = "https://osos.pythonanywhere.com";
-export const apiUrl = "https://osos.pythonanywhere.com/api/v1";
+export const baseUrl = "https://osos.pythonanywhere.com"
+export const apiUrl = "https://osos.pythonanywhere.com/api/v1"
 
 export const dvbaseUrl = "https://bugbounty.pythonanywhere.com";
 export const dvApiUrl = "https://bugbounty.pythonanywhere.com/api/v1";
