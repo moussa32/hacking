de605e4a2fe600df01238ac7756b69eeb2659ae7
Moussa <moussaebrahem32@gmail.com>
2021-07-28 20:19:03 +0200

    Fixed navbar bug

--- src/modules/Blog/components/layout/Navbar.js
+++ src/modules/Blog/components/layout/Navbar.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { handleGetUserToken } from '../../actions/index';
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 
-import { GreenLogo } from "../../../../assets/index";
+import {GreenLogo} from "../../../../assets/index";
 
-const Navbar = ({ currentPathname }) => {
+const Navbar = ({currentPathname}) => {
   const [isOpen, setIsOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("home");
-  let token = handleGetUserToken();
 
   useEffect(() => {
     if (currentPathname.includes("/blog")) {
@@ -30,64 +28,36 @@ const Navbar = ({ currentPathname }) => {
       <Link className="navbar-brand" to="/">
         <img src={GreenLogo} width={30} height={30} alt="logo" loading="lazy" />
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        onClick={toggle}
-      >
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" onClick={toggle}>
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div
-        className="collapse navbar-collapse"
-        id="navbarNav"
-        style={{ display: isOpen ? "block" : "none" }}
-      >
+      <div className="collapse navbar-collapse" id="navbarNav" style={{display: isOpen ? "block" : "none"}}>
         <ul className="navbar-nav">
-          <li
-            className={`nav-item ${activeTab === "home" ? "active" : ""}`}
-            id="home"
-          >
+          <li className={`nav-item ${activeTab === "home" ? "active" : ""}`} id="home">
             <Link className="nav-link" to="/">
               الرئيسية <span className="sr-only">(current)</span>
             </Link>
           </li>
-          <li
-            className={`nav-item ${activeTab === "blog" ? "active" : ""}`}
-            id="blog"
-          >
+          <li className={`nav-item ${activeTab === "blog" ? "active" : ""}`} id="blog">
             <Link className="nav-link" to="/blog">
               المدونة
             </Link>
           </li>
-          <li
-            className={`nav-item ${activeTab === "about-us" ? "active" : ""}`}
-            id="about-us"
-          >
+          <li className={`nav-item ${activeTab === "about-us" ? "active" : ""}`} id="about-us">
             <Link className="nav-link" to="/about-us">
               من نحن
             </Link>
           </li>
-          <li
-            className={`nav-item ${activeTab === "contact-us" ? "active" : ""}`}
-            id="contact-us"
-          >
+          <li className={`nav-item ${activeTab === "contact-us" ? "active" : ""}`} id="contact-us">
             <Link className="nav-link" to="/contact-us">
               اتصل بنا
             </Link>
           </li>
         </ul>
         <ul className="navbar-nav mr-auto">
-          <li
-            className={`nav-item ${activeTab === "login" ? "active" : ""}`}
-            id="login"
-          >
+          <li className={`nav-item ${activeTab === "login" ? "active" : ""}`} id="login">
             <Link className="nav-link" to="/login">
-              {token ? 'تسجيل الدخول' : 'لوحة التحكم'}
+              {localStorage.getItem("accessToken") ? "لوحة التحكم" : "تسجيل الدخول"}
             </Link>
           </li>
         </ul>
