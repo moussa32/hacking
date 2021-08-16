edd32a7a1d4ce890181b1e4d7f44e1ffcacaadb8
Moussa <moussaebrahem32@gmail.com>
2021-08-16 13:16:18 +0200

    Fix white spaces in available programs

--- src/modules/AvailablePrograms.js
+++ src/modules/AvailablePrograms.js
@@ -110,134 +110,128 @@ const AvailablePrograms = props => {
   return (
     <>
       {token ? choseNavbar() : <Navbar currentPathname={props.location.pathname} />}
-      <mian className="component-wrapper h-100">
+      <div className="component-wrapper h-100">
         {loadded ? (
           <div className="container-fluid home available-margin">
-            <div className="row">
-              <div className="jumbotron jumbotron-fluid text-center col-12 py-4 bg-second rounded">
-                <div className="container-fluid">
-                  <div className="bg-black">
-                    <div className="jumbotron jumbotron-fluid bg-black rounded py-1">
-                      <div className="container px-4">
-                        <Swiper {...params}>
-                          {programAds && programAds.length > 0
-                            ? programAds.map(ad => {
-                                return (
-                                  <div key={ad.id}>
-                                    <img src={ad.image} className="d-block w-100" alt={ad.title} />
-                                    <div className="carousel-caption d-none d-md-block">
-                                      <h4>{ad.title}</h4>
-                                      <p className="lead">{ad.description.length > 150 ? ad.description.substring(0, 150) + " ......." : ad.description}</p>
-                                      {ad.ad_link && (
-                                        <a href={ad.ad_link} target="_blank" className="btn btn-lightgreen">
-                                          قراءة المزيد
-                                        </a>
-                                      )}
-                                    </div>
-                                  </div>
-                                );
-                              })
-                            : null}
-                        </Swiper>
-                      </div>
-                    </div>
-                    <div className="jumbotron jumbotron-fluid bg-black rounded py-4">
-                      <div className="container px-4">
-                        <>
-                          <div className="row">
-                            <div className="col-md-4">
-                              <CustomSelect
-                                id="program-type"
-                                options={[
-                                  { value: "opened", label: "مفتوح" },
-                                  { value: "closed", label: "مغلق" },
-                                  { value: "eligable", label: "مدفوع" },
-                                ]}
-                                isClearable={true}
-                                isSearchable={true}
-                                onChange={onProductStatus}
-                                placeholder="نوع البرنامج"
-                                classNames="form-group w-100 categories-filters"
-                              />
-                            </div>
-                            <div className="col-md-4 mr-auto">
-                              <CustomSelect
-                                id="product-type"
-                                options={[
-                                  { value: "Windows", label: "Windows" },
-                                  { value: "IOS", label: "IOS" },
-                                  { value: "Android", label: "Android" },
-                                  { value: "dm", label: "Domain Name" },
-                                ]}
-                                isClearable={true}
-                                isSearchable={true}
-                                onChange={onProductType}
-                                placeholder="نوع المنتج / الأصل"
-                                classNames="form-group w-100 categories-filters"
-                              />
-                            </div>
-                          </div>
-                          {!programs || programs.length === 0 ? (
-                            <>
-                              <div className="alert alert-warning text-center w-100" role="alert">
-                                لا يوجد أي برامج متاحة
+            <div className="container bg-second p-4">
+              <div className="jumbotron jumbotron-fluid bg-black rounded py-1">
+                <div className="container px-4">
+                  <Swiper {...params}>
+                    {programAds && programAds.length > 0
+                      ? programAds.map(ad => {
+                          return (
+                            <div key={ad.id}>
+                              <img src={ad.image} className="d-block w-100" alt={ad.title} />
+                              <div className="carousel-caption d-none d-md-block">
+                                <h4>{ad.title}</h4>
+                                <p className="lead">{ad.description.length > 150 ? ad.description.substring(0, 150) + " ......." : ad.description}</p>
+                                {ad.ad_link && (
+                                  <a href={ad.ad_link} target="_blank" className="btn btn-lightgreen">
+                                    قراءة المزيد
+                                  </a>
+                                )}
                               </div>
-                            </>
-                          ) : (
-                            <div className="row section-container px-0">
-                              {programs.map(program => {
-                                return (
-                                  <div key={program.id} className="col-xl-4 col-md-4 p-3 rounded">
-                                    <a href={`/program/${program.id}/${program.company_name}`} className="card bg-second border-0 program-card text-white text-decoration-none">
-                                      <div className="card-header program-card-header">
-                                        <img className="card-img-top program-logo p-0 d-block mx-auto" src={`${program.logo}`} alt={program.name} />
-                                      </div>
-                                      <div className="card-body d-flex flex-row-reverse justify-content-between align-items-center py-0">
-                                        <h3 className="card-title badge-name my-3 text-left text-capitalize">{program.company_name}</h3>
-                                        {program.status === "opened" ? (
-                                          <span>
-                                            <FaDollarSign size={"1.2rem"} color={"#009cde"} />
-                                          </span>
-                                        ) : null}
-                                      </div>
-                                      <div className="card-body py-2 d-flex flex-row-reverse justify-content-between program-text">
-                                        <p className="text-lightgreen card-text">
-                                          $
-                                          {Math.min.apply(
-                                            Math,
-                                            program.bounty_bars.map(function (o) {
-                                              return o.amount;
-                                            })
-                                          )}{" "}
-                                          - $
-                                          {Math.max.apply(
-                                            Math,
-                                            program.bounty_bars.map(function (o) {
-                                              return o.amount;
-                                            })
-                                          )}
-                                        </p>
-                                        <p className="card-text">{convertDate(program.launch_date)}</p>
-                                      </div>
-                                    </a>
-                                  </div>
-                                );
-                              })}
                             </div>
-                          )}
-                        </>
+                          );
+                        })
+                      : null}
+                  </Swiper>
+                </div>
+              </div>
+              <div className="jumbotron jumbotron-fluid bg-black rounded py-4">
+                <div className="container px-4">
+                  <>
+                    <div className="row">
+                      <div className="col-md-4">
+                        <CustomSelect
+                          id="program-type"
+                          options={[
+                            { value: "opened", label: "مفتوح" },
+                            { value: "closed", label: "مغلق" },
+                            { value: "eligable", label: "مدفوع" },
+                          ]}
+                          isClearable={true}
+                          isSearchable={true}
+                          onChange={onProductStatus}
+                          placeholder="نوع البرنامج"
+                          classNames="form-group w-100 categories-filters"
+                        />
+                      </div>
+                      <div className="col-md-4 mr-auto">
+                        <CustomSelect
+                          id="product-type"
+                          options={[
+                            { value: "Windows", label: "Windows" },
+                            { value: "IOS", label: "IOS" },
+                            { value: "Android", label: "Android" },
+                            { value: "dm", label: "Domain Name" },
+                          ]}
+                          isClearable={true}
+                          isSearchable={true}
+                          onChange={onProductType}
+                          placeholder="نوع المنتج / الأصل"
+                          classNames="form-group w-100 categories-filters"
+                        />
                       </div>
                     </div>
-                  </div>
+                    {!programs || programs.length === 0 ? (
+                      <>
+                        <div className="alert alert-warning text-center w-100" role="alert">
+                          لا يوجد أي برامج متاحة
+                        </div>
+                      </>
+                    ) : (
+                      <div className="row section-container px-0">
+                        {programs.map(program => {
+                          return (
+                            <div key={program.id} className="col-xl-4 col-md-4 p-3 rounded">
+                              <a href={`/program/${program.id}/${program.company_name}`} className="card bg-second border-0 program-card text-white text-decoration-none">
+                                <div className="card-header program-card-header">
+                                  <img className="card-img-top program-logo p-0 d-block mx-auto" src={`${program.logo}`} alt={program.name} />
+                                </div>
+                                <div className="card-body d-flex flex-row-reverse justify-content-between align-items-center py-0">
+                                  <h3 className="card-title badge-name my-3 text-left text-capitalize">{program.company_name}</h3>
+                                  {program.status === "opened" ? (
+                                    <span>
+                                      <FaDollarSign size={"1.2rem"} color={"#009cde"} />
+                                    </span>
+                                  ) : null}
+                                </div>
+                                <div className="card-body py-2 d-flex flex-row-reverse justify-content-between program-text">
+                                  <p className="text-lightgreen card-text">
+                                    $
+                                    {Math.min.apply(
+                                      Math,
+                                      program.bounty_bars.map(function (o) {
+                                        return o.amount;
+                                      })
+                                    )}{" "}
+                                    - $
+                                    {Math.max.apply(
+                                      Math,
+                                      program.bounty_bars.map(function (o) {
+                                        return o.amount;
+                                      })
+                                    )}
+                                  </p>
+                                  <p className="card-text">{convertDate(program.launch_date)}</p>
+                                </div>
+                              </a>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </>
                 </div>
               </div>
             </div>
-            <Footer />
           </div>
         ) : (
           <Spinner></Spinner>
         )}
-      </mian>
+      </div>
+      <Footer />
     </>
   );
 };
