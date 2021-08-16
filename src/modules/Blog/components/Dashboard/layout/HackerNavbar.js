6ef80fec3cc3b0d68f77bda05dd56991ceaa90f0
Moussa <moussaebrahem32@gmail.com>
2021-08-16 16:52:27 +0200

    Fix white spaces in dashboard and settings

--- src/modules/Blog/components/Dashboard/layout/HackerNavbar.js
+++ src/modules/Blog/components/Dashboard/layout/HackerNavbar.js
@@ -49,75 +49,77 @@ const HackerNavbar = props => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark blog-nav">
-      {loadded ? (
-        <>
-          <Link className="navbar-brand" to="/">
-            <img src={WhiteLogo} width={30} height={30} alt="logo" loading="lazy" />
-          </Link>
-          <button className="navbar-toggler navbar-user-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <ul className="navbar-nav mr-auto sub-hacker-list">
-            <li className="nav-item nav-icon" id="messages">
-              <Link className="nav-link disabled" to="/dashboard/leaderboard">
-                <MdEmail className="text-lightgreen" size={"1.8rem"} />
-              </Link>
-            </li>
-            <li className="nav-item nav-icon" id="alerts">
-              <Link className="nav-link disabled" to="/dashboard/leaderboard">
-                <BsBellFill className="text-lightgreen" size={"1.5rem"} />
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle d-sm-inline-block border-0 bg-transparent" id="hacker-profile" data-toggle="dropdown" aria-expanded="false">
-                <img src={navbarInfo !== null && navbarInfo.hacker.avater !== null ? `${dvbaseUrl}/${navbarInfo.hacker.avater}` : `${DefaultAvatar}`} className="hacker-avatar img-fluid rounded-circle mr-1" alt={navbarInfo !== null && navbarInfo.username ? navbarInfo.username : "مخترق"} />
-                <IoIosArrowDown className="text-lightgreen mr-2" size={"1.3rem"} />
-              </button>
-              <div className={`dropdown-menu text-right ml-3`}>
-                <a className="dropdown-item hacker-options disabled" href="pages-profile.html">
-                  <FaUserAlt className="ml-2" /> الصفحة الشخصية
-                </a>
-                <a className="dropdown-item hacker-options" href="/dashboard/settings">
-                  <BsFillGearFill className="ml-2" /> الإعدادات
-                </a>
-                <button onClick={handleLogout} className="dropdown-item hacker-options border-0 bg-transparent" href="/#">
-                  <FaSignOutAlt className="ml-2" />
-                  تسجيل الخروج
-                </button>
-              </div>
-            </li>
-          </ul>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <ul className="navbar-nav">
-              <li className={`nav-item ${activeTab === "activity" ? "active" : ""}`} id="activity">
-                <Link className="nav-link disabled" to="/dashboard/activity">
-                  النشاط <span className="sr-only">(current)</span>
-                </Link>
-              </li>
-              <li className={`nav-item ${activeTab === "available-programs" ? "active" : ""}`} id="available-programs">
-                <Link className="nav-link" to={`/available-programs`}>
-                  البرامج المتاحة
-                </Link>
-              </li>
-              <li className={`nav-item ${activeTab === "main" ? "active" : ""}`} id="main">
-                <Link className="nav-link" to="/dashboard">
-                  لوحة التحكم
-                </Link>
-              </li>
-              <li className={`nav-item ${activeTab === "leaderboard" ? "active" : ""}`} id="leaderboard">
+      <div className="container">
+        {loadded ? (
+          <>
+            <Link className="navbar-brand" to="/">
+              <img src={WhiteLogo} width={30} height={30} alt="logo" loading="lazy" />
+            </Link>
+            <button className="navbar-toggler navbar-user-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <ul className="navbar-nav mr-auto sub-hacker-list">
+              <li className="nav-item nav-icon" id="messages">
                 <Link className="nav-link disabled" to="/dashboard/leaderboard">
-                  لوحة القادة
+                  <MdEmail className="text-lightgreen" size={"1.8rem"} />
                 </Link>
               </li>
-              <li className={`nav-item ${activeTab === "" ? "active" : ""}`} id="">
-                <Link className="nav-link disabled" to="/">
-                  التسليمات
+              <li className="nav-item nav-icon" id="alerts">
+                <Link className="nav-link disabled" to="/dashboard/leaderboard">
+                  <BsBellFill className="text-lightgreen" size={"1.5rem"} />
                 </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <button className="nav-link dropdown-toggle d-sm-inline-block border-0 bg-transparent" id="hacker-profile" data-toggle="dropdown" aria-expanded="false">
+                  <img src={navbarInfo !== null && navbarInfo.hacker.avater !== null ? `${dvbaseUrl}/${navbarInfo.hacker.avater}` : `${DefaultAvatar}`} className="hacker-avatar img-fluid rounded-circle mr-1" alt={navbarInfo !== null && navbarInfo.username ? navbarInfo.username : "مخترق"} />
+                  <IoIosArrowDown className="text-lightgreen mr-2" size={"1.3rem"} />
+                </button>
+                <div className={`dropdown-menu text-right ml-3`}>
+                  <a className="dropdown-item hacker-options disabled" href="pages-profile.html">
+                    <FaUserAlt className="ml-2" /> الصفحة الشخصية
+                  </a>
+                  <a className="dropdown-item hacker-options" href="/dashboard/settings">
+                    <BsFillGearFill className="ml-2" /> الإعدادات
+                  </a>
+                  <button onClick={handleLogout} className="dropdown-item hacker-options border-0 bg-transparent" href="/#">
+                    <FaSignOutAlt className="ml-2" />
+                    تسجيل الخروج
+                  </button>
+                </div>
               </li>
             </ul>
-          </div>
-        </>
-      ) : null}
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <ul className="navbar-nav">
+                <li className={`nav-item ${activeTab === "activity" ? "active" : ""}`} id="activity">
+                  <Link className="nav-link disabled" to="/dashboard/activity">
+                    النشاط <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "available-programs" ? "active" : ""}`} id="available-programs">
+                  <Link className="nav-link" to={`/available-programs`}>
+                    البرامج المتاحة
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "main" ? "active" : ""}`} id="main">
+                  <Link className="nav-link" to="/dashboard">
+                    لوحة التحكم
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "leaderboard" ? "active" : ""}`} id="leaderboard">
+                  <Link className="nav-link disabled" to="/dashboard/leaderboard">
+                    لوحة القادة
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "" ? "active" : ""}`} id="">
+                  <Link className="nav-link disabled" to="/">
+                    التسليمات
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </>
+        ) : null}
+      </div>
     </nav>
   );
 };
--- src/modules/Program/components/layout/Navbar.js
+++ src/modules/Program/components/layout/Navbar.js
@@ -55,71 +55,73 @@ const Navbar = ({ currentPathname }) => {
     <>
       {navbarInfo ? (
         <nav className="navbar navbar-expand-lg navbar-dark blog-nav">
-          <Link className="navbar-brand" to="/">
-            <img src={WhiteLogo} width={30} height={30} alt="logo" loading="lazy" />
-          </Link>
-          <button className="navbar-toggler navbar-user-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <ul className="navbar-nav">
-              <li className={`nav-item ${activeTab === "activity" ? "active" : ""}`} id="activity">
-                <Link className="nav-link disabled" to="/program/dashboard/activity">
-                  النشاط <span className="sr-only">(current)</span>
+          <div className="container">
+            <Link className="navbar-brand" to="/">
+              <img src={WhiteLogo} width={30} height={30} alt="logo" loading="lazy" />
+            </Link>
+            <button className="navbar-toggler navbar-user-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <ul className="navbar-nav">
+                <li className={`nav-item ${activeTab === "activity" ? "active" : ""}`} id="activity">
+                  <Link className="nav-link disabled" to="/program/dashboard/activity">
+                    النشاط <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "available-programs" ? "active" : ""}`} id="available-programs">
+                  <Link className="nav-link" to="/available-programs">
+                    البرامج المتاحة
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "main" ? "active" : ""}`} id="main">
+                  <Link className="nav-link" to="/program/dashboard">
+                    لوحة التحكم
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "leaderboard" ? "active" : ""}`} id="leaderboard" disabled>
+                  <Link className="nav-link disabled" to="/program/dashboard/leaderboard">
+                    لوحة القادة
+                  </Link>
+                </li>
+                <li className={`nav-item ${activeTab === "" ? "active" : ""}`} id="deliverables" disabled>
+                  <Link className="nav-link disabled" to={`${match.path}/`}>
+                    التسليمات
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <ul className="navbar-nav mr-auto sub-hacker-list">
+              <li className="nav-item nav-icon" id="messages">
+                <Link className="nav-link disabled" to={`${match.path}/leaderboard`}>
+                  <MdEmail className="text-lightgreen" size={"1.8rem"} />
                 </Link>
               </li>
-              <li className={`nav-item ${activeTab === "available-programs" ? "active" : ""}`} id="available-programs">
-                <Link className="nav-link" to="/available-programs">
-                  البرامج المتاحة
+              <li className="nav-item nav-icon" id="alerts">
+                <Link className="nav-link disabled" to={`${match.path}/leaderboard`}>
+                  <BsBellFill className="text-lightgreen" size={"1.5rem"} />
                 </Link>
               </li>
-              <li className={`nav-item ${activeTab === "main" ? "active" : ""}`} id="main">
-                <Link className="nav-link" to="/program/dashboard">
-                  لوحة التحكم
-                </Link>
-              </li>
-              <li className={`nav-item ${activeTab === "leaderboard" ? "active" : ""}`} id="leaderboard" disabled>
-                <Link className="nav-link disabled" to="/program/dashboard/leaderboard">
-                  لوحة القادة
-                </Link>
-              </li>
-              <li className={`nav-item ${activeTab === "" ? "active" : ""}`} id="deliverables" disabled>
-                <Link className="nav-link disabled" to={`${match.path}/`}>
-                  التسليمات
-                </Link>
+              <li className="nav-item dropdown">
+                <button className="nav-link dropdown-toggle d-sm-inline-block border-0 bg-transparent" id="hacker-profile" data-toggle="dropdown" aria-expanded="false">
+                  <img src={navbarInfo.program && navbarInfo.program.logo !== null ? `${dvbaseUrl}/${navbarInfo.program.logo}` : DefaultAvatar} className="hacker-avatar img-fluid rounded-circle mr-1" alt={navbarInfo && navbarInfo.program !== null ? navbarInfo.program.username : "User image"} />
+                  <IoIosArrowDown className="text-lightgreen mr-2" size={"1.3rem"} />
+                </button>
+                <div className={`dropdown-menu text-right ml-3`}>
+                  <a className="dropdown-item hacker-options disabled" href="pages-profile.html">
+                    <FaUserAlt className="ml-2" /> الصفحة الشخصية
+                  </a>
+                  <a className="dropdown-item hacker-options" href={`/program/dashboard/settings`}>
+                    <BsFillGearFill className="ml-2" /> الإعدادات
+                  </a>
+                  <button onClick={handleLogout} className="dropdown-item hacker-options border-0 bg-transparent" href="#">
+                    <FaSignOutAlt className="ml-2" />
+                    تسجيل الخروج
+                  </button>
+                </div>
               </li>
             </ul>
           </div>
-          <ul className="navbar-nav mr-auto sub-hacker-list">
-            <li className="nav-item nav-icon" id="messages">
-              <Link className="nav-link disabled" to={`${match.path}/leaderboard`}>
-                <MdEmail className="text-lightgreen" size={"1.8rem"} />
-              </Link>
-            </li>
-            <li className="nav-item nav-icon" id="alerts">
-              <Link className="nav-link disabled" to={`${match.path}/leaderboard`}>
-                <BsBellFill className="text-lightgreen" size={"1.5rem"} />
-              </Link>
-            </li>
-            <li className="nav-item dropdown">
-              <button className="nav-link dropdown-toggle d-sm-inline-block border-0 bg-transparent" id="hacker-profile" data-toggle="dropdown" aria-expanded="false">
-                <img src={navbarInfo.program && navbarInfo.program.logo !== null ? `${dvbaseUrl}/${navbarInfo.program.logo}` : DefaultAvatar} className="hacker-avatar img-fluid rounded-circle mr-1" alt={navbarInfo && navbarInfo.program !== null ? navbarInfo.program.username : "User image"} />
-                <IoIosArrowDown className="text-lightgreen mr-2" size={"1.3rem"} />
-              </button>
-              <div className={`dropdown-menu text-right ml-3`}>
-                <a className="dropdown-item hacker-options disabled" href="pages-profile.html">
-                  <FaUserAlt className="ml-2" /> الصفحة الشخصية
-                </a>
-                <a className="dropdown-item hacker-options" href={`/program/dashboard/settings`}>
-                  <BsFillGearFill className="ml-2" /> الإعدادات
-                </a>
-                <button onClick={handleLogout} className="dropdown-item hacker-options border-0 bg-transparent" href="#">
-                  <FaSignOutAlt className="ml-2" />
-                  تسجيل الخروج
-                </button>
-              </div>
-            </li>
-          </ul>
         </nav>
       ) : null}
     </>
